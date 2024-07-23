8d9b823919c10c20813359b233dad10a44142b78
YanZhao0719 <redalert0719@gmail.com>
2024-07-23 14:39:10 +0800

    add code

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/pricing', function () {
+    return view('pricing');
+});
+
+Route::get('/faq', function () {
+    return view('faq');
+});
+
+Route::get('/blog-home', function () {
+    return view('blog-home');
+});
+
+Route::get('/blog-post', function () {
+    return view('blog-post');
+});
+
+Route::get('/portfolio-item', function () {
+    return view('portfolio-item');
+});
+
+Route::get('/portfolio-overview', function () {
+    return view('portfolio-overview');
+});
